3fa82478d8aa18fbd4e64695156b8331e5b5c9dc
Mathias Schoettle <39705097+MathiasSchoettle@users.noreply.github.com>
2020-02-28 16:38:16 +0100

    added ice spear and arcane shield as spells

--- Assets/Scripts/Spells/Spellbook.cs
+++ Assets/Scripts/Spells/Spellbook.cs
@@ -10,9 +10,9 @@ public class Spellbook
         this.Player = player;
 
         // TODO add spells to Spellbook in a different Way, not just in plain code, maybe by using file system?
-        AllSpells.Add(new FireBallSpell("fireballSpell", "Fireball", Resources.Load("FireBallSpell") as GameObject));
-        AllSpells.Add(new FireBallSpell("iceSpearSpell", "Ice Spear", Resources.Load("FireBallSpell") as GameObject));
-        AllSpells.Add(new FireBallSpell("shieldSpell", "Arcane Shield", Resources.Load("FireBallSpell") as GameObject));
+        AllSpells.Add(new FireBallSpell("fireballSpell", "Fireball", Resources.Load("FireBallPrefab") as GameObject));
+        AllSpells.Add(new FireBallSpell("iceSpearSpell", "Ice Spear", Resources.Load("IceSpearPrefab") as GameObject));
+        AllSpells.Add(new FireBallSpell("shieldSpell", "Arcane Shield", Resources.Load("ArcaneShieldPrefab") as GameObject));
 
         LearnSpell(AllSpells[0].GetID());
         SelectSpell(AllSpells[0].GetID());

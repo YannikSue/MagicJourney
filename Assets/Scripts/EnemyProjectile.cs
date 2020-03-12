72288723939e7a0b00360ec289bfb148ab567e28
Yannik Südbeck <y.suedbeck@yahoo.de>
2020-03-12 13:01:49 +0100

    started calculating the launchangle etc

--- Assets/Scripts/EnemyProjectile.cs
+++ Assets/Scripts/EnemyProjectile.cs
@@ -7,10 +7,22 @@ public class EnemyProjectile : MonoBehaviour
 
     public float speed;
     private Rigidbody2D rb;
+    private GameObject player;
 
 
     void Start()
     {
+        player = GameObject.FindWithTag("Player");
+
+        //formel für Flugbahn
+        //Grad = arctan(y/x + Mathf.sqr("y²/(x²+1))
+        //float x = player.transform.position.x;
+        //float y = player.transform.position.y;
+        //float erg = x / y + Mathf.Sqrt(Mathf.Pow(y, 2) / (Mathf.Pow(x, 2) + 1);
+        //float launchAngle = Mathf.Atan2(erg);
+        //"Launchspeed fehlt"
+        //Vectorberechnung fehlt Vector2 arrowDirection = new Vector2(Mathf.cos(launchAngle), Mathf.sin(launchAngle));
+
         rb = gameObject.GetComponent<Rigidbody2D>();
         rb.AddForce(Vector2.left * 10, ForceMode2D.Impulse);
     }

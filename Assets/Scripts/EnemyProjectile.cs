2f83eebe41b7895d1aeeb52b396d645445f91529
Yannik Südbeck <y.suedbeck@yahoo.de>
2020-03-12 16:02:19 +0100

    calculation kinda works now

--- Assets/Scripts/EnemyProjectile.cs
+++ Assets/Scripts/EnemyProjectile.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class EnemyProjectile : MonoBehaviour
 {
 
-    public float speed;
+    public float speed = 5f;
     private Rigidbody2D rb;
     private GameObject player;
 
@@ -16,15 +16,19 @@ public class EnemyProjectile : MonoBehaviour
 
         //formel für Flugbahn
         //Grad = arctan(y/x + Mathf.sqr("y²/(x²+1))
-        //float x = player.transform.position.x;
-        //float y = player.transform.position.y;
-        //float erg = x / y + Mathf.Sqrt(Mathf.Pow(y, 2) / (Mathf.Pow(x, 2) + 1);
-        //float launchAngle = Mathf.Atan2(erg);
+        float x = player.transform.position.x;
+        float y = player.transform.position.y;
+        float erg = x / y + Mathf.Sqrt(Mathf.Pow(y, 2) / (Mathf.Pow(x, 2) + 1));
+        float launchAngle = Mathf.Atan(erg) * Mathf.Rad2Deg + 90;
         //"Launchspeed fehlt"
         //Vectorberechnung fehlt Vector2 arrowDirection = new Vector2(Mathf.cos(launchAngle), Mathf.sin(launchAngle));
 
+        float launchSpeed = Mathf.Sqrt(Mathf.Pow(x,2) + Mathf.Pow(y,2));
+        float Theta = Mathf.Atan(y / x) + 90;
+        Vector2 launchVector = new Vector2(Mathf.Cos(Theta), Mathf.Sin(Theta));
+
         rb = gameObject.GetComponent<Rigidbody2D>();
-        rb.AddForce(Vector2.left * 10, ForceMode2D.Impulse);
+        rb.AddForce(launchVector * speed, ForceMode2D.Impulse);
     }
 
     
